1271074d6bd59ad836c41b29d3438b93214a3633
Robert Schmoltzi <robert.schmoltzi@warp9.de>
2021-04-27 22:03:44 +0200

    Added split and cmd methods to Util

--- test/helpers/Util.cpp
+++ test/helpers/Util.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "Util.h"
+#include <string>
+#include <sstream>
+#include <algorithm>
+
+std::string Util::getOutputOfBashCommand(const std::string &cmd) {
+    std::string data;
+    FILE *      stream;
+    const int   max_buffer = 256;
+    char        buffer[max_buffer];
+    auto manipCommand = cmd;
+    manipCommand.append(" 2>&1");
+
+    stream = popen(manipCommand.c_str(), "r");
+
+    if (stream) {
+        while (!feof(stream))
+            if (fgets(buffer, max_buffer, stream) != NULL) data.append(buffer);
+        pclose(stream);
+    }
+    return data;
+}
+
+std::vector<std::string> Util::split(const std::string& str, const char delim) {
+    std::vector<std::string> ret;
+    std::stringstream ss(str);
+    std::string token;
+    while (std::getline(ss, token, delim)) {
+        ret.push_back(token);
+    }
+    return ret;
+}
--- test/helpers/Util.h
+++ test/helpers/Util.h
@@ -6,11 +6,16 @@
 #define CONCURRENT_GROWTABLE_UTIL_H
 
 
+#include <string>
+#include <vector>
 
 class Util {
-
+public:
+    /* Takes a string and executes it as a bash command. The output is returned as string.*/
+    static std::string getOutputOfBashCommand(const std::string& cmd);
+    /* Definitely should not be part of any standard library */
+    static std::vector<std::string> split(const std::string& str, const char delim);
 };
-
 
 
 #endif //CONCURRENT_GROWTABLE_UTIL_H
