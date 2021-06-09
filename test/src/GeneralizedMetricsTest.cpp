b6fb468008a8ca02c4de856525e98653a397404a
Tobias Bodmer <Tobias.Bodmer@gmx.de>
2021-06-09 14:49:25 +0200

    only calc a single tree combination per sample

--- test/src/GeneralizedMetricsTest.cpp
+++ test/src/GeneralizedMetricsTest.cpp
@@ -38,6 +38,10 @@ TEST_CASE("MCI compare un-normalized", "[MCI][un-normalized][long]") {
 	test_metric(base_path, base_path_res, metr);
 }
 static void write_to_file(const std::string &file, const std::string &content);
+static std::string
+combine_trees_to_file(const std::string &source_tree_file, size_t tree_idx_a, size_t tree_idx_b);
+static bool nearly_eq_floating(double a, double b);
+
 TEST_CASE("Calculate simple trees", "[dbg]") {
 	// Edit your trees to test here
 	std::string tree1 = "((((F,C),B),A),(E,D));";
@@ -63,18 +67,27 @@ TEST_CASE("Calculate simple trees", "[dbg]") {
 static void test_metric(const std::string &base_path_splits,
                         const std::string &base_path_res,
                         const RfMetricInterface::Metric &metric) {
-	// For each result, run our <metric> algorithm on the first 10 trees
+	std::random_device rand_dev;
+	std::mt19937 rnd(rand_dev());
+	std::uniform_int_distribution tree_idx_distr(0, 9);
+	// For each result, run our <metric> algorithm on two (random) contained trees
 	for (const auto &entry : std::filesystem::directory_iterator(base_path_res)) {
 		// skip irrelevant files
 		if (entry.path().filename() == "." || entry.path().filename() == "..") {
 			continue;
 		}
 		CHECK(entry.is_directory());
-
 		std::string res_fname = entry.path().filename();
+
+		// generate problem instance between only two trees from data
+		size_t tree_idx_a = tree_idx_distr(rnd);
+		size_t tree_idx_b = tree_idx_distr(rnd);
+		std::string trees_path =
+		    combine_trees_to_file(base_path_splits + res_fname, tree_idx_a, tree_idx_b);
+
 		// Perform calculation on problem instance
 		RfMetricInterface::Params params;
-		params.input_file_path = base_path_splits + res_fname;
+		params.input_file_path = trees_path;
 		params.metric = metric;
 		params.normalize_output = false;
 		INFO("This might indicate an exception inside the metrics calculations.")
@@ -82,18 +95,60 @@ static void test_metric(const std::string &base_path_splits,
 		CHECK(!res_fname.empty());
 		RfMetricInterface iface(params);
 		iface.do_magical_high_performance_stuff();
-		io::IOData res = iface.get_result_as_IOData();
+		auto res = iface.get_result().pairwise_similarities;
 
 		// load solution matrix
 		RectMatrix<double> true_mtx =
 		    Util::parse_mtx_from_r(base_path_res + res_fname + "/pairwise_trees");
-		REQUIRE(true_mtx.is_symmetric());
-        auto symmetric_true_mtx = true_mtx.to_symmetric_mtx();
-		io::IOData true_ioData;
-		true_ioData.pairwise_distance_mtx = symmetric_true_mtx.to_vector();
-		INFO("Comparing ours to reference")
-		CHECK(true_ioData.comparePairwiseDistances(res));
+        REQUIRE(true_mtx.is_symmetric());
+		// compare tree score with it self
+        INFO("Comparing ours to reference")
+        CHECK(nearly_eq_floating(true_mtx.at(tree_idx_a, tree_idx_a), res.at(0, 0)));
+		CHECK(nearly_eq_floating(true_mtx.at(tree_idx_b, tree_idx_b), res.at(1, 1)));
+		// compare across trees
+        CHECK(nearly_eq_floating(true_mtx.at(tree_idx_a, tree_idx_b), res.at(1, 0)));
+
 	}
+}
+static std::string
+combine_trees_to_file(const std::string &source_tree_file, size_t tree_idx_a, size_t tree_idx_b) {
+	// essentially write line idx_a and idx_b from source to a output file
+	auto idx_a = tree_idx_a <= tree_idx_b ? tree_idx_a : tree_idx_b;
+	auto idx_b = tree_idx_a <= tree_idx_b ? tree_idx_b : tree_idx_a;
+	std::string a_str, b_str;
+
+	std::ifstream ifstream(source_tree_file);
+	REQUIRE(ifstream.is_open());
+	std::string line_str;
+	size_t line_nr = 0;
+	// skip lines until equal
+	for (; line_nr <= idx_a; ++line_nr) {
+		std::getline(ifstream, line_str);
+	}
+	a_str = line_str;
+	if (idx_a != idx_b) {
+		// skip until b
+		for (; line_nr <= idx_b; ++line_nr) {
+			std::getline(ifstream, line_str);
+		}
+		b_str = line_str;
+	} else {
+		b_str = a_str;
+	}
+	ifstream.close();
+	// write to temporary file
+	std::string tmp_file_path = "/tmp/pairw_tree_instance";
+	std::ofstream out(tmp_file_path);
+	REQUIRE(out.is_open());
+	// dont accidentally reorder trees
+	if (tree_idx_a > tree_idx_b) {
+		out << b_str << "\n" << a_str << "\n";
+
+	} else {
+		out << a_str << "\n" << b_str << "\n";
+	}
+	out.close();
+	return tmp_file_path;
 }
 
 static void write_to_file(const std::string &file, const std::string &content) {
@@ -103,4 +158,12 @@ static void write_to_file(const std::string &file, const std::string &content) {
 	}
 	ostr << content;
 	ostr.close();
+}
+
+bool nearly_eq_floating(double a, double b) {
+	auto absA = std::abs(a);
+	auto absB = std::abs(b);
+	auto largest = (absA < absB) ? absB : absA;
+	auto smallest = (absA < absB) ? absA : absB;
+	return largest - smallest <= largest * static_cast<double>(FLT_EPSILON) * 1e5;
 }
