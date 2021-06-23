54fdc2c5091426b26185bae8b67dca04d50ccbd0
Robert Schmoltzi <robert.schmoltzi@warp9.de>
2021-06-23 21:06:50 +0200

    Adds tests for quick timing

---
+++ test/src/TimingTests.cpp
@@ -0,0 +1,50 @@
+//
+// Created by Robert Schmoltzi on 23.06.21.
+//
+// Class to quickly check the runtime impact of changes. Danger of overfitting to specific trees.
+// Tests should run through pretty fast. They might not work properly with enabled compiler
+// optimizations because results can be unused. Importantly, the trees on which the metric is
+// calculated are not randomized.
+
+#include <RfMetricInterface.h>
+#include <catch2/catch.hpp>
+
+static void time_metric(const RfMetricInterface::Metric &metric);
+
+TEST_CASE("SPI timing test", "[SPI][Time]")
+{
+    auto metr = RfMetricInterface::SPI;
+    time_metric(metr);
+}
+
+TEST_CASE("MSI timing test", "[MSI][Time]")
+{
+    auto metr = RfMetricInterface::MSI;
+    time_metric(metr);
+}
+
+TEST_CASE("MCI timing test", "[MCI][Time]")
+{
+	auto metr = RfMetricInterface::MCI;
+	time_metric(metr);
+}
+
+static void time_metric(const RfMetricInterface::Metric &metric) {
+	std::string base_path = "../test/samples/data/heads/BS/";
+	std::vector<std::string> trees = {"125", "141", "350"};
+
+	for (const auto &ts : trees) {
+		// Perform calculation on problem instance
+		RfMetricInterface::Params params;
+		params.input_file_path = base_path + ts;
+		params.metric = metric;
+		params.normalize_output = false;
+		INFO("Calculating distances from " + ts);
+		CHECK(!ts.empty());
+		RfMetricInterface iface(params);
+		iface.do_magical_high_performance_stuff();
+		auto res = iface.get_result().pairwise_similarities;
+
+		// TODO: do smth with res
+	}
+}
