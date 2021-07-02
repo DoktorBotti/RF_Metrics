02bd2e8c2d152b301bd1f663ad69be5dbcb035ae
Tobias Bodmer <Tobias.Bodmer@gmx.de>
2021-07-02 17:05:09 +0200

    logging max used index in DblFacCache

--- src/rf/LogDblFact.cpp
+++ src/rf/LogDblFact.cpp
@@ -1,4 +1,6 @@
 #include "LogDblFact.h"
+#include <boost/log/attributes/constant.hpp>
+#include <boost/log/sources/record_ostream.hpp>
 #include <cassert>
 #include <cmath>
 
@@ -6,20 +8,32 @@ size_t LogDblFact::get_capacity() const {
 	return num_reserved;
 }
 LogDblFact::Scalar LogDblFact::lg_fast(size_t x) const {
+	if (countMaxUsage && x > max_called_lg) {
+		max_called_lg = x;
+	}
 	assert(x < num_reserved);
 	return cache_lg[x];
 }
 [[maybe_unused]] LogDblFact::Scalar LogDblFact::lg_dbl_fact_fast(long x) const {
+	if (countMaxUsage && x > max_called_dbl_fac) {
+		max_called_dbl_fac = x;
+	}
 	assert(static_cast<size_t>(std::abs(x)) < num_reserved);
 	assert(x >= -1);
 	return cache_fac[static_cast<size_t>(x + 1)];
 }
 LogDblFact::Scalar LogDblFact::lg_rooted_dbl_fact_fast(long x) const {
+	if (countMaxUsage && x > max_called_dbl_fac) {
+		max_called_dbl_fac = x;
+	}
 	assert(x >= 2);
 	assert(static_cast<size_t>(x + x - 2) < num_reserved);
 	return cache_fac[static_cast<size_t>(x + x - 2)];
 }
 LogDblFact::Scalar LogDblFact::lg_unrooted_dbl_fact_fast(long x) const {
+	if (countMaxUsage && x > max_called_dbl_fac) {
+		max_called_dbl_fac = x;
+	}
 	assert(x >= 1);
 	assert(static_cast<size_t>(x + x - 4) < num_reserved);
 	return cache_fac[static_cast<size_t>(x + x - 4)];
@@ -33,12 +47,18 @@ void LogDblFact::reserve(size_t num_els) {
 	calc_vals(num_els);
 }
 LogDblFact::Scalar LogDblFact::lg(size_t x) {
+	if (countMaxUsage && x > max_called_lg) {
+		max_called_lg = x;
+	}
 	if (num_reserved <= x) {
 		reserve(static_cast<size_t>(x) * GROW_RATE);
 	}
 	return lg_fast(x);
 }
 LogDblFact::Scalar LogDblFact::lg_dbl_fact(long x) {
+	if (countMaxUsage && x > max_called_dbl_fac) {
+		max_called_dbl_fac = x;
+	}
 	assert(x > -2);
 	if (num_reserved <= static_cast<size_t>(std::abs(x))) {
 		reserve(static_cast<size_t>(x) * GROW_RATE);
@@ -46,6 +66,9 @@ LogDblFact::Scalar LogDblFact::lg_dbl_fact(long x) {
 	return lg_dbl_fact_fast(x);
 }
 LogDblFact::Scalar LogDblFact::lg_rooted_dbl_fact(long x) {
+	if (countMaxUsage && x > max_called_rooted) {
+		max_called_rooted = x;
+	}
 	assert(x > -2);
 	if (num_reserved <= static_cast<size_t>(std::abs(x + x - 2))) {
 		reserve(static_cast<size_t>(x + x - 2) * GROW_RATE); // TODO: make (x - 1) << 1
@@ -54,8 +77,11 @@ LogDblFact::Scalar LogDblFact::lg_rooted_dbl_fact(long x) {
 }
 LogDblFact::Scalar LogDblFact::lg_unrooted_dbl_fact(long x) {
 	assert(x > -2);
+	if (countMaxUsage && x > max_called_unrooted) {
+		max_called_unrooted = x;
+	}
 	if (num_reserved <= static_cast<size_t>(std::abs(x + x - 4))) {
-        reserve(static_cast<size_t>(x + x - 4) * GROW_RATE); // TODO: make (x - 2) << 1
+		reserve(static_cast<size_t>(x + x - 4) * GROW_RATE); // TODO: make (x - 2) << 1
 	}
 	return lg_unrooted_dbl_fact_fast(x);
 }
@@ -71,6 +97,8 @@ LogDblFact::LogDblFact() noexcept : cache_fac(PRECALC_NUM_ELEMS), cache_lg(PRECA
 	cache_lg[2] = 1;
 	// calc factorials and log from here
 	calc_vals(PRECALC_NUM_ELEMS);
+	logger.add_attribute("Tag",
+	                     boost::log::attributes::constant<std::string>("DoubleFac_Lg_Cache"));
 }
 void LogDblFact::calc_vals(size_t new_num_els) {
 	for (size_t x = 3; x < new_num_els; ++x) {
@@ -79,4 +107,15 @@ void LogDblFact::calc_vals(size_t new_num_els) {
 		cache_fac[x] = cache_fac[x - 2] + cache_lg[x - 1];
 	}
 	num_reserved = new_num_els;
+}
+void LogDblFact::printLog() {
+	if (countMaxUsage) {
+		BOOST_LOG_SEV(logger, lg::notification)
+		    << "Reserved " << num_reserved << " elements. Maximum lg called was " << max_called_lg
+		    << " Max unrooted: " << max_called_unrooted << " (=>"
+		    << max_called_unrooted + max_called_unrooted - 4 << ")"
+		    << " Max rooted: " << max_called_rooted << " ("
+		    << max_called_rooted + max_called_rooted - 2 << ")"
+		    << " Max dbl fac: " << max_called_dbl_fac << " (=>" << max_called_dbl_fac + 1 << ")";
+	}
 }
--- src/rf/include/LogDblFact.h
+++ src/rf/include/LogDblFact.h
@@ -1,15 +1,20 @@
 #ifndef INFORF_LOGDBLFACT_H
 #define INFORF_LOGDBLFACT_H
+#include <boost/log/sources/severity_logger.hpp>
+#include "LoggingBackend.h"
 #include <cstdio>
 #include <vector>
 // Class caches and calculates log2(x!!) and log2(x)
 // it automatically manages its (instance dependent) cache size and offers different helpers for
 // different offsets in the arguments
 // TODO: should the cache be static & global? -> experiment in next phase
+
 class LogDblFact {
   public:
+	const bool countMaxUsage = true;
 	typedef double Scalar;
 	LogDblFact() noexcept;
+	void printLog();
 	[[nodiscard]] size_t get_capacity() const;
 	void reserve(size_t num_els);
 	// fast function do not check array boundaries. Caller must guarantee enough capacity
@@ -30,13 +35,19 @@ class LogDblFact {
 	[[nodiscard]] Scalar lg_unrooted_dbl_fact(long x);
 
   private:
+	mutable size_t max_called_lg = 0;
+	mutable long max_called_unrooted = 0;
+	mutable long max_called_dbl_fac = 0;
+	mutable long max_called_rooted = 0;
 	const static size_t PRECALC_NUM_ELEMS = 100;
 	constexpr static size_t GROW_RATE = 2;
 	size_t num_reserved = PRECALC_NUM_ELEMS;
 	// since -1!! exists, the array must start for x=-1
 	std::vector<Scalar> cache_fac;
 	std::vector<Scalar> cache_lg;
-	void calc_vals(size_t new_num_els);
+    boost::log::sources::severity_logger<lg::SeverityLevel> logger;
+
+    void calc_vals(size_t new_num_els);
 };
 
 #endif // INFORF_LOGDBLFACT_H
