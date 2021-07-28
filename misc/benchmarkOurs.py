b6099a000a9e7800ced2ec83cd6aeecf47619918
Tobias Bodmer <Tobias.Bodmer@gmx.de>
2021-07-28 14:15:04 +0200

    optimizing benchmarking skript

--- misc/benchmarkOurs.py
+++ misc/benchmarkOurs.py
@@ -1,7 +1,7 @@
 import os, subprocess, time
 import numpy as np
 # normal configuration variables
-test_files_dir = '/rf_metrics/BS/'
+test_files_dir = '/home/cme_practical_team_1/data/BS/'
 project_dir = os.path.pardir
 tmp_dir = "/home/cme_practical_team_1/tmp/"
 our_exe = project_dir + "/bin/commandline_rf"
@@ -10,10 +10,18 @@ metrics = ["MCI", "MSI", "SPI"]
 timeout_in_secs = 60 * 60 # 1 hour
 test_names = [file for file in os.listdir(test_files_dir)]
 test_paths = [test_files_dir + a for a in test_names]
+###
+# Special values in numpy matrix:
+#  0 := not even written to. -> program probably terminated early
+# -1 := the program exited with an error code
+# -2 := the program did not terminate, the timeout got triggered
+# -3 := instance too hard, the instance with less trees did not make it, this wont either
+# -4 := instance probably too hard, a different metric in this configuration failed. -Just optimizing number of times we hit timeouts
 
 # configure which testset to execute
 upper_taxa_bound = 1000000
-tree_counts  = [2,10,100,500,1000] # all measurements will be performed with these counts
+# all measurements will be performed with these counts
+tree_counts  = [10,100,500,1000, 5000, 7500, 10000] 
 # test matrix dimensions [treeNum][taxa/testfile][metric]
 
 def writeToFile(file_names, ours_times):
@@ -63,14 +71,26 @@ try:
                 print("Value error while reading taxa count. Skipping")
                 continue
         # create temporary file which holds correct number of trees
+        failed_prev_tree = False
         for trees_idx, num_trees in enumerate(tree_counts):
             with open(path, "r") as all_trees:
                 with open(tmp_dir + "tmp.trees", "w") as out_trees:
                     head = [next(all_trees) for x in range(num_trees)]
                     out_trees.writelines(head)
+            # check if should skip because of trees
+            if failed_prev_tree:
+                # write -3 to all metrics which will be skipped
+                ours_times[trees_idx, num] = -3.
+                continue
             # temporaries which would not print into main array if something fails
             ours_metric_arr =  np.zeros(shape=(len(metrics),))
             for m_idx, metric in enumerate(metrics):
+                if failed_prev_tree :
+                    # is the case when the previous metric ran into a timeout. Since runtimes and needed storage are comarable, 
+                    # we skip this calculation too.
+                    ours_metric_arr[m_idx] = -4.
+                    continue
+
                 status_string = f"processing {metric} in {test_names[num]}, {num+1} of {len(test_paths)}, with {num_trees} trees. "
                 # run our script
                 ours_args = [our_exe, '--metric', metric, '-o', tmp_dir + 'ourRes.txt', '-i', tmp_dir + "tmp.trees"]
@@ -85,10 +105,12 @@ try:
                 except subprocess.TimeoutExpired as exc:
                     print("Timeout!")
                     runtime = -2.
+                    failed_prev_tree = True
                 # writing to array upon successful calculation
                 if proc != 0:
                     print("error occured!")
                     ours_metric_arr[m_idx] = -1.
+                    failed_prev_tree = True
                 else:
                     ours_metric_arr[m_idx] = runtime
 
